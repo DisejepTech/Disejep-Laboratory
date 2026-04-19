f13c33bd85c54b604e39a30bf287dc246b8ae202
DisejepTech <jepart21@gmail.com>
2026-04-18 22:24:53 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Marca Registrada: OJ")
+st.write("Si puedes leer esto, es porque ya nos pusimos los zapatos.")
+st.balloons() # Esto lanzará globos en la pantalla al cargar
